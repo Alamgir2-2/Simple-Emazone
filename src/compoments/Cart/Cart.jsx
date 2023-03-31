9bcb61b30e4d0b8aeb1a1035eecf37eb4e6f8b53
Alamgir2-2 <darkthehunter22@gmail.com>
2023-04-01 00:12:00 +0600

    show data from cart

--- src/compoments/Cart/Cart.jsx
+++ src/compoments/Cart/Cart.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import './Cart.css'
-const Cart = ({cart}) => {
-// const cart = props.cart; //option 1
-// const{cart}=props; //option 2
+const Cart = ({ cart }) => {
+    // const cart = props.cart; //option 1
+    // const{cart}=props; //option 2
 
-let total = 0;
-let totalShipping = 0;
-for(const product of cart){
-    total = total + product.price;
-    totalShipping = totalShipping + product.shipping;
-}
+    let total = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
 
-const tax = total*7/100;
-const grandTotal = total + tax +totalShipping;
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1;
+
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+
+    const tax = total * 7 / 100;
+    const grandTotal = total + tax + totalShipping;
 
     return (
         <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: <strong>$</strong>{total}</p>
             <p>Total Shipping: <strong>$</strong>{totalShipping}</p>
             <p>Tax: <strong>$</strong>{tax}</p>
-            <h6>Grand Total: <strong>$</strong>{grandTotal}</h6>
+            <h6>Grand Total: <strong>$</strong>{grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
